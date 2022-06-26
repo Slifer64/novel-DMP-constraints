3e3f90de4dc4acb826efea02f9d06630e83a4adb
Slifer <antosidi64@yahoo.gr>
2022-06-27 01:52:45 +0300

    3D ellipsoid and plane draw

--- simulations/plot_ellipsoids_test.m
+++ simulations/plot_ellipsoids_test.m
@@ -5,26 +5,6 @@ clear;
 % rng(0);
 
 
-load('data/obs_avoid_data')
-
-
-ellipsoid{1} = struct('Sigma',getEllipseSigma(45, 0.15, 0.08), 'c',[-0.2; 0.3]);
-ellipsoid{2} = struct('Sigma',getEllipseSigma(-45, 0.1, 0.06), 'c',[0.16; 0.57]);
-ellipsoid_colors = {[1.0, 0.4, 0], [0.4, 0.2, 0]};
-
-for i=1:length(ellipsoid)  
-    E_p{i} = drawElipsoid2D(ellipsoid{i}.Sigma, ellipsoid{i}.c);
-end
-
-figure; hold on;
-plot(Pd_data(1, :), Pd_data(2, :), 'LineWidth',2);
-for i=1:length(E_p)
-    plot(E_p{i}(1,:), E_p{i}(2,:), 'LineWidth',2, 'Color',ellipsoid_colors{i});
-    plot(ellipsoid{i}.c(1), ellipsoid{i}.c(2), 'LineWidth',2, 'Marker','x', 'LineStyle','None', 'Markersize',14, 'Color',ellipsoid_colors{i});
-end
-axis equal
-
-
 %% Define Ellipsoid
 
 % theta = 45 * pi / 180;
@@ -33,54 +13,91 @@ axis equal
 % Sigma = R * Lambda_2.^2 * R';
 % c = [0.5; 0.2];
 
-theta = rand() * pi / 180;
-R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
-Lambda_2 = diag([2*rand(), 2*rand()]);
+R = eul2rotm([30, 20, 90]);
+Lambda_2 = diag([1, 2, 0.5]);
 Sigma = R * Lambda_2.^2 * R';
-c = [rand(); rand()];
+c = [0; 0; 0];
 
 %% Define a point inside the ellipsoid
-r = 0.2 + 0.6*rand();
-theta = rand()*2*pi;
-p1 = c + chol(Sigma, 'lower')*r*[cos(theta); sin(theta)];
+% r = 0.2 + 0.6*rand();
+% theta = rand()*2*pi;
+% phi = rand()*pi;
+% p1 = c + chol(Sigma, 'lower')*r*[sin(phi)*cos(theta); sin(phi)*sin(theta); cos(phi)];
+p1 = [-0.0393; 0.1753; -0.3047];
 
 %% Find the point on the surface ellipsoid
 lambda = 1 / sqrt((p1-c)'/Sigma*(p1-c));
-p2 = c + lambda*(p1-c);
+p_e = c + lambda*(p1-c);
 
 %% Find the norm to the ellipsoid on that point
-n = Sigma \ (p2 - c);
+n = Sigma \ (p_e - c);
 n = n / norm(n);
 
 %% calculate two points on tangent line
-v = [n(2); - n(1)]; % tangent line direction
-p3 = p2 - 0.4*v;
-p4 = p2 + 0.4*v;
+v = [n(2); - n(1); 0]; % tangent line direction
+v = v / norm(v);
+% p3 = p2 - 0.4*v;
+% p4 = p2 + 0.4*v;
+
+[x_plane, y_plane, z_plane] = createPlane(p_e, n, 0.5);
+
 
 %% plot
-p = drawElipsoid2D(Sigma, c);
+[X, Y, Z] = drawElipsoid(Sigma, c);
 
 figure; hold on;
-plot(p(1,:), p(2,:), 'LineWidth',2, 'Color','blue')
-quiver(p2(1), p2(2), n(1), n(2), 0.5, 'Color','green', 'LineWidth',3);
-plot([p3(1) p4(1)], [p3(2) p4(2)], 'LineWidth',2.2, 'LineStyle','-.', 'Color',[0 0.8 0]);
-plot([c(1) p2(1)], [c(2) p2(2)], 'LineWidth',2, 'LineStyle',':', 'Color','cyan');
-plot(c(1), c(2), 'LineWidth',2, 'Marker','x', 'LineStyle','None', 'Markersize',14, 'Color','blue');
-plot(p1(1), p1(2), 'LineWidth',2, 'Marker','x', 'LineStyle','None', 'Markersize',14, 'Color','red');
-plot(p2(1), p2(2), 'LineWidth',2, 'Marker','x', 'LineStyle','None', 'Markersize',14, 'Color','magenta');
+sh = surf(X, Y, Z, 'LineStyle','None', 'FaceColor',[0.85 0.33 0.1], 'FaceAlpha',0.5);
+patch_h = patch(x_plane, y_plane, z_plane, 'green', 'FaceAlpha',0.2, 'LineStyle','none');
+quiver3(p_e(1), p_e(2), p_e(3), n(1), n(2), n(3), 0.5, 'Color','green', 'LineWidth',3);
+% plot3([p3(1) p4(1)], [p3(2) p4(2)], 'LineWidth',2.2, 'LineStyle','-.', 'Color',[0 0.8 0]);
+plot3([c(1) p_e(1)], [c(2) p_e(2)], [c(3) p_e(3)], 'LineWidth',2, 'LineStyle',':', 'Color','cyan');
+plot3(c(1), c(2), c(3),  'LineWidth',3, 'Marker','*', 'LineStyle','None', 'Markersize',16, 'Color','blue');
+plot3(p1(1), p1(2), p1(3), 'LineWidth',3, 'Marker','x', 'LineStyle','None', 'Markersize',16, 'Color','red');
+plot3(p_e(1), p_e(2), p_e(3), 'LineWidth',3, 'Marker','x', 'LineStyle','None', 'Markersize',16, 'Color','magenta');
 axis equal
+view(-148.33, 5.3529);
 
 
 %% ===================================
 %% ===================================
 
-function p = drawElipsoid2D(Sigma, c)
+function [X, Y, Z] = drawElipsoid(Sigma, c)
 
-    theta = linspace(0, 2*pi, 200);
+    theta = linspace(0, 2*pi, 20);
+    phi = linspace(0, pi, 20);
+    
+    [Theta, Phi] = meshgrid(theta, phi);
+    
     L = chol(Sigma, 'lower'); % sqrtm(Sigma);
-    p = c + L*[cos(theta); sin(theta)];
+    X = sin(Phi).*cos(Theta);
+    Y = sin(Phi).*sin(Theta);
+    Z = cos(Phi);
+    
+    P = [X(:)'; Y(:)'; Z(:)'];
+    
+    size(P)
+    size(L)
+    P = c + L*P;
+    
+    X = reshape(P(1,:), length(theta), length(phi));
+    Y = reshape(P(2,:), length(theta), length(phi));
+    Z = reshape(P(3,:), length(theta), length(phi));
+    
+end
+
+function [X, Y, Z] = createPlane(center, normal, scale)
+    
+    % Ax + By + Cz + D = 0
+    A = normal(1);
+    B = normal(2);
+    C = normal(3);
+    D = -dot(normal, center);
+    X = center(1) + scale*[1 -1 -1 1]; % Generate data for x vertices
+    Y = center(2) + scale*[1 1 -1 -1]; % Generate data for y vertices
+    Z = -1/C*(A*X + B*Y + D); % Solve for z vertices data
 
 end
+
 
 function Sigma = getEllipseSigma(angle, lambda_x, lambda_y)
 
